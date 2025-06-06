41cbf7306121b4b07f02677975a4c492fb37f151
Vivekmunjyasara <munjyasaravivek@gmail.com>
2025-06-06 19:53:03 +0530

    6 june evening

---
+++ SUPERGLOBALS/submitserver.php
@@ -0,0 +1,28 @@
+<?php
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // collect value of input field
+            $name = htmlspecialchars($_REQUEST['fname']);
+            if (empty($name)) {
+                echo "Name is empty";
+            } else {
+                echo $name;
+            }
+        }
+        if($_SERVER["REQUEST_METHOD"]=="GET")
+        {
+            echo 'hell';
+             $name = htmlspecialchars($_REQUEST['fname']);
+             if (empty($name)) {
+                echo "Name is empty";
+            } if($name == "bhagyashree choudhary") {
+                echo "bhagyashree NARESH choudhary";
+            }
+            else
+            {
+                echo $name;
+            }
+        }
+?>
+
+
---
+++ SUPERGLOBALS/supergloabl_server.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+        echo $_SERVER['PHP_SELF'];
+        echo "<br>";
+        echo $_SERVER['SERVER_NAME'];
+        echo "<br>";
+        echo $_SERVER['HTTP_HOST'];
+        echo "<br>";
+        echo $_SERVER['HTTP_USER_AGENT'];
+        echo "<br>";
+        echo $_SERVER['SCRIPT_NAME'];
+        ?>
+
+        <br><br><hr>
+        <form method="get" action="submitserver.php">
+            Name: <input type="text" name="fname">
+            <input  type="submit">
+            
+        </form>
+
+       
+    </body>
+</html>
