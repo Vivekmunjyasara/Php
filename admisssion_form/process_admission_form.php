c433f06156bec96021bdfcec0d6bd862a9011dcb
Vivekmunjyasara <munjyasaravivek@gmail.com>
2025-07-29 21:02:14 +0530

    chances

--- admisssion_form/process_admission_form.php
+++ admisssion_form/process_admission_form.php
@@ -3,16 +3,29 @@
 class accept_data {
 
     public $fname, $mname, $lname, $phone, $email, $gen, $eqa, $lan, $per, $pass, $wing, $build, $area, $city, $land, $pin, $dob, $bg, $country, $state, $ref, $img;
-    public $datapdo, $localhost, $dbname, $con, $query;
+    public $datapdo, $localhost, $dbname, $con, $query,$target_dir,$target_file,$imageFileType,$stmt;
 
     function upload_data() {
         $this->localhost = "127.0.0.1";
         $this->dbname = "studata";
+        
         try {
-            $this->datapdo = new PDO("mysql:host=$this->localhost;dbname=$this->dbname;","root","");
-            $this->query="insert into admission_form(first_name,middle_name, last_name, phone, email, gender, education, language, percentage, passing_year, wing, building, area, city, landmark, pincode, dob, blood_grp, country, state, refrence,img) values('".$this->fname."','".$this->mname."','".$this->lname."',".$this->phone.",'".$this->email."','".$this->gen."','".$this->eqa."','".$this->lan."',".$this->per.",".$this->pass.",'".$this->wing."','".$this->build."','".$this->area."','".$this->city."','".$this->land."',".$this->pin.",'".$this->dob."','".$this->bg."','".$this->country."','".$this->state."','".$this->ref."',".$this->img.")";
+           if (isset($_FILES['photo'])) 
+           {
+                $this->target_dir = "C:\form_image/uploads/";
+                $this->target_file = $this->target_dir . basename($_FILES["photo"]["name"]);
+                $this->imageFileType = strtolower(pathinfo($this->target_file, PATHINFO_EXTENSION));
+           }
+           $this->datapdo = new PDO("mysql:host=$this->localhost;dbname=$this->dbname;","root","");
+             $this->stmt =$this->datapdo->prepare("INSERT INTO admission_form (image) VALUES ($this->target_file)");
+             $this->stmt->bindParam('image','?');
+             $this->stmt->execute();
+    
+             
+            $this->query="insert into admission_form(first_name,middle_name, last_name, phone, email, gender, education, language, percentage, passing_year, wing, building, area, city, landmark, pincode, dob, blood_grp, country, state, refrence) values('".$this->fname."','".$this->mname."','".$this->lname."',".$this->phone.",'".$this->email."','".$this->gen."','".$this->eqa."','".$this->lan."',".$this->per.",".$this->pass.",'".$this->wing."','".$this->build."','".$this->area."','".$this->city."','".$this->land."',".$this->pin.",'".$this->dob."','".$this->bg."','".$this->country."','".$this->state."','".$this->ref."')";
             $this->datapdo->exec($this->query);  
-        }       
+           
+        }           
         catch (Exception $e) 
             {
             echo $e;
@@ -38,9 +51,9 @@ class accept_data {
         $this->land = empty(filter_input(INPUT_POST,'land', FILTER_SANITIZE_SPECIAL_CHARS))?"LAND PENDING":filter_input(INPUT_POST,'land', FILTER_SANITIZE_SPECIAL_CHARS);
        $this->pin = empty(filter_input(INPUT_POST,'pin', FILTER_SANITIZE_NUMBER_INT))?"NO PINCODE":filter_input(INPUT_POST,'pin', FILTER_SANITIZE_NUMBER_INT);
         $this->dob = empty(filter_input(INPUT_POST,'dob', FILTER_SANITIZE_SPECIAL_CHARS))?"NO DOB":filter_input(INPUT_POST,'dob', FILTER_SANITIZE_SPECIAL_CHARS);
-       $this->bg = empty(filter_input(INPUT_POST,'bg', FILTER_SANITIZE_STRING))?"NO BLOOD GROUP SELECTED":filter_input(INPUT_POST,'bg', FILTER_SANITIZE_STRING);
-       $this->country = empty(filter_input(INPUT_POST,'country', FILTER_SANITIZE_STRING))?"COUNTRY NOT SELECTED":filter_input(INPUT_POST,'country', FILTER_SANITIZE_STRING);
-        $this->state = empty(filter_input(INPUT_POST,'state', FILTER_SANITIZE_STRING))?"STATE NOT SELECTED":filter_input(INPUT_POST,'state', FILTER_SANITIZE_STRING);
+       $this->bg = empty(filter_input(INPUT_POST,'bg', FILTER_SANITIZE_SPECIAL_CHARS))?"NO BLOOD GROUP SELECTED":filter_input(INPUT_POST,'bg', FILTER_SANITIZE_SPECIAL_CHARS);
+       $this->country = empty(filter_input(INPUT_POST,'country', FILTER_SANITIZE_SPECIAL_CHARS))?"COUNTRY NOT SELECTED":filter_input(INPUT_POST,'country', FILTER_SANITIZE_SPECIAL_CHARS);
+        $this->state = empty(filter_input(INPUT_POST,'state', FILTER_SANITIZE_SPECIAL_CHARS ))?"STATE NOT SELECTED":filter_input(INPUT_POST,'state', FILTER_SANITIZE_SPECIAL_CHARS);
         $this->ref = empty(filter_input(INPUT_POST,'ref', FILTER_SANITIZE_SPECIAL_CHARS))?"NO REFERENCE":filter_input(INPUT_POST,'ref', FILTER_SANITIZE_SPECIAL_CHARS);
         $this->img= $_POST['photo'];
         }
